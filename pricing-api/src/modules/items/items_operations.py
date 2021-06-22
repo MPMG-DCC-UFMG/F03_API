6829b35d25ea905a80824982b6b697e1c1f264b0
marscos <marcoscarvalhodeassis@gmail.com>
2021-06-22 20:15:42 -0300

    List items endpoint, using ilike for descriptions, non-exhaustive params

--- pricing-api/src/modules/items/items_operations.py
+++ pricing-api/src/modules/items/items_operations.py
@@ -1,4 +1,6 @@
-from typing import Optional
+from typing import Optional, List
+
+from sqlalchemy.sql.sqltypes import Date
 from fastapi import Query
 
 class ListItemsQueryParams:
@@ -7,9 +9,15 @@ class ListItemsQueryParams:
         limit: int = Query(0, description="Query limit"),
         offset: int = Query(15, description="Query offset"),
         sort: Optional[str] = Query(None,description="The sorting attribute"),
-        description: Optional[str] = Query(None, description="Item name"),
+        county: Optional[str] = Query(None,description="Município do órgão licitante"),
+        year: Optional[List[str]] = Query(None, description="Ano da licitação"),
+        description: Optional[str] = Query(None, description="Texto na descrição original do item"),
+        group: Optional[str] = Query(None, description="Nome do grupo do item")
     ):
         self.limit = limit
         self.offset = offset
         self.sort = sort
+        self.county = county
+        self.year = year
         self.description = description
+        self.group = group
--- pricing-api/src/modules/items/items_repository.py
+++ pricing-api/src/modules/items/items_repository.py
@@ -1,9 +1,26 @@
 from src.modules.items.items_operations import ListItemsQueryParams
+from src.modules.items.item import ItemModel
+from src.db.database import db_session
+from sqlalchemy import and_
 
 class ItemsRepository:
     
     def find_by_id(id:str):
-        return {"id": 1, "description": "máscara pff2", "units_of_measures": ["unidade", "pacote"]}
+        result = db_session.query(ItemModel).get(id)
+        return result.__dict__
     
     def list(params: ListItemsQueryParams):
-        return [{"id": 1, "description": "máscara pff2", "units_of_measures": ["unidade", "pacote"]}]
+
+        filters = []
+        # Ideia: passar a construção dessa lista filters para o construtor da classe ListItemsQueryParams.
+        if (params.county):
+            filters.append(ItemModel.municipio == params.county)   
+        if (params.year):
+            filters.append(ItemModel.ano.in_(params.year))
+        if (params.description):
+            filters.append(ItemModel.ilike("%" + params.description + "%"))
+        if (params.group):
+            filters.append(ItemModel.grupo == params.group)
+
+        result = db_session.query(ItemModel).filter(and_(*filters))[params.offset:params.offset+params.limit]
+        return [ row.__dict__ for row in result ]
