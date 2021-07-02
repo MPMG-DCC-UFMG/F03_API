23d779a95784eb89ed355fa75780c5ad4aab5e0e
marscos <marcoscarvalhodeassis@gmail.com>
2021-07-02 01:13:38 -0300

    Simple pricing endpoint

--- pricing-api/src/modules/pricing/pricing_operations.py
+++ pricing-api/src/modules/pricing/pricing_operations.py
@@ -5,11 +5,12 @@ from fastapi import Query
 class PricingQueryParams:
     def __init__(
         self,
-        description: str = Query(..., description="The item description"),
-        year: Optional[int] = Query(..., description="The desired year"),
-        units_of_measure: Optional[List[str]] = Query(
-            None, description="Selected units of measure"),
+        description: Optional[str] = Query(None, description="The item description"),
+        year: Optional[int] = Query(None, description="The desired year"),
+        units_of_measure: Optional[List[str]] = Query(None, description="Selected units of measure"),
+        group: Optional[str] = Query(None, description="The group name")
     ):
         self.description = description
         self.year = year
         self.units_of_measure = units_of_measure
+        self.group = group
--- pricing-api/src/modules/pricing/pricing_service.py
+++ pricing-api/src/modules/pricing/pricing_service.py
@@ -1,6 +1,8 @@
+from typing import List
 from src.modules.pricing.pricing_operations import PricingQueryParams
+from src.modules.pricing.pricing_repository import PricingRepository as repository
 
 class PricingsService:
 
     def get(params: PricingQueryParams):
-        print('Should be implemented')
+        return repository.get(params)
