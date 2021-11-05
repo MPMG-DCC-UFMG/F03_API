0a6ad016b94a9bc2d67d8f771cbbd63c5bdebb16
pedrobrum <pedrobrum@dcc.ufmg.br>
2021-11-05 10:02:56 -0300

    Using offset and limit parameters

--- pricing-api/src/modules/filters/filters_repository.py
+++ pricing-api/src/modules/filters/filters_repository.py
@@ -7,22 +7,20 @@ class FilterRepository:
     def list(params: ListFiltersQueryParams):
         filters = []
         column = getattr(ItemModel, params.column)
-        
+
         if params.prefix:
             prefix = f"{params.prefix}%"
             filters.append(column.ilike(prefix))
         if params.contains:
             contains = f"%{params.contains}%"
             filters.append(column.ilike(contains))
-        
+
         query = db_session\
             .query(column)\
             .distinct()\
             .filter(and_(*filters))\
             .order_by(desc(params.column))\
-            .offset(0)\
-            .limit(30)
-
-        print(query)
+            .offset(params.offset)\
+            .limit(params.limit)
 
         return [x[0] for x in query.all()]
