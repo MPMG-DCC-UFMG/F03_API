cca478eb46bc5b7501fd45cef7df18ae01d7cb3b
Matheus Cândido Teixeira <matheuscandido2009@gmail.com>
2021-11-04 22:55:22 -0400

    Add filters

---
+++ pricing-api/src/modules/filters/filter_schema.py
@@ -0,0 +1,11 @@
+from typing import Optional, Any
+from pydantic import BaseModel, Field
+
+class Filter(BaseModel):
+    licitacao: str = Field(example="123", title='Identificador da licitaçao')
+    grupo: str = Field(example="gasolina_0", title="Grupo a qual o item faz parte")
+    dsc_unidade_medida: str = Field(example="litro", title="Unidade de medida do objeto na licitação correspondente")
+    ano: int = Field(example=2015, title="Ano em que a licitação foi homologada")
+    municipio: str = Field(example="Belo Horizonte", title='Município em que a licitação ocorreu')
+    orgao: str = Field(example="Prefeitura de Belo Horizonte", title='Órgão responsável por iniciar o processo de licitação')
+    primeiro_termo: str = Field(example="gasolina", title='Primeiro termo da descrição do item')
---
+++ pricing-api/src/modules/filters/filters_repository.py
@@ -0,0 +1,28 @@
+from src.modules.filters.filters_operations import ListFiltersQueryParams
+from src.modules.items.item import ItemModel
+from src.db.database import db_session
+from sqlalchemy import and_, desc, asc, func
+
+class FilterRepository:
+    def list(params: ListFiltersQueryParams):
+        filters = []
+        column = getattr(ItemModel, params.column)
+        
+        if params.prefix:
+            prefix = f"{params.prefix}%"
+            filters.append(column.ilike(prefix))
+        if params.contains:
+            contains = f"%{params.contains}%"
+            filters.append(column.ilike(contains))
+        
+        query = db_session\
+            .query(column)\
+            .distinct()\
+            .filter(and_(*filters))\
+            .order_by(desc(params.column))\
+            .offset(0)\
+            .limit(30)
+
+        print(query)
+
+        return [x[0] for x in query.all()]
