d59b8dc74b5d5014d4c85f6e7f1e1e033d092f9b
pedrobrum <pedrobrum@dcc.ufmg.br>
2021-12-06 17:14:09 -0300

    Added offset and limit params to query

--- pricing-api/src/modules/items/items_repository.py
+++ pricing-api/src/modules/items/items_repository.py
@@ -4,54 +4,75 @@ from src.db.database import db_session, es
 from sqlalchemy import and_, desc, asc
 from sqlalchemy.orm import load_only
 
+
 class ItemsRepository:
 
-    def find_by_id(id:str):
+    def find_by_id(id: str):
         result = db_session.query(ItemModel).get(id)
         return result.__dict__
 
     def list(params: ListItemsQueryParams):
         filters = params.filters
-        filters.append(ItemModel.item_ruido == 0) # Recupera apenas os itens que não são ruído.
-
-        QUERY = {
-            "_source": False,
-            "query": {
-                "bool": {
-                  "must": {
-                    "match": {
-                      "original": params.description
-                    }
-                  }
-                }
-            }
-        }
+        # Recupera apenas os itens que não são ruído.
+        filters.append(ItemModel.item_ruido == 0)
 
         if params.description:
-            result = es.search(index="item", body=QUERY)
+            QUERY = {
+                "_source": False,
+                "query": {
+                    "bool": {
+                        "must": {
+                            "match": {
+                                "original": params.description
+                            }
+                        }
+                    }
+                }
+            }
+            result = es.search(index="item", body=QUERY, from_=params.offset,
+                               size=params.limit)
             hits = result["hits"]["hits"]
-            ids = [int(d["_id"]) for d in hits]
+            ids = [d["_id"] for d in hits]
             filters.append(ItemModel.item_id.in_(ids))
-            params.reset_description() # para evitar de ser filtrado novamente
-
-        # TODO: retornar os ids dos itens
-
-        # TODO: buscar os itens na tabela do druid que possuem os id retornados
-        # pelo elasticsearch
 
         order = desc(params.sort) if params.order == "desc" else asc(params.sort)
-        result = db_session.query(ItemModel).filter(and_(*filters)).order_by(order)
-        result = result[params.offset:params.offset+params.limit]
-        return [ row.__dict__ for row in result ]
+        result = db_session.query(ItemModel)\
+                           .filter(and_(*filters))\
+                           .order_by(order)
+
+        return [row.__dict__ for row in result]
 
     def list_sample(params: ListItemsQueryParams):
         filters = params.filters
-        filters.append(ItemModel.item_ruido == 0) # Recupera apenas os itens que não são ruído.
+        # Recupera apenas os itens que não são ruído.
+        filters.append(ItemModel.item_ruido == 0)
 
-        fields = ['original', 'original_dsc', 'dsc_unidade_medida', 'grupo', 'data', \
-                  'modalidade', 'tipo_licitacao', 'nome_vencedor', 'orgao', \
+        if params.description:
+            QUERY = {
+                "_source": False,
+                "query": {
+                    "bool": {
+                        "must": {
+                            "match": {
+                                "original": params.description
+                            }
+                        }
+                    }
+                }
+            }
+            result = es.search(index="item", body=QUERY, from_=params.offset,
+                               size=params.limit)
+            hits = result["hits"]["hits"]
+            ids = [d["_id"] for d in hits]
+            filters.append(ItemModel.item_id.in_(ids))
+
+        fields = ['original', 'original_dsc', 'dsc_unidade_medida', 'grupo', 'data',
+                  'modalidade', 'tipo_licitacao', 'nome_vencedor', 'orgao',
                   'municipio', 'qtde_item', 'preco']
         order = desc(params.sort) if params.order == "desc" else asc(params.sort)
-        result = db_session.query(ItemModel).filter(and_(*filters)).options(load_only(*fields)).order_by(order)[params.offset:params.offset+params.limit]
+        result = db_session.query(ItemModel)\
+                           .filter(and_(*filters))\
+                           .options(load_only(*fields))\
+                           .order_by(order)
 
-        return [ row.__dict__ for row in result ]
+        return [row.__dict__ for row in result]
