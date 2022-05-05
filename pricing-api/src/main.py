8fb5a1524a81bf21ccca6a269c0867617f6c3f46
Mariana Silva <marianaossilva@gmail.com>
2022-05-05 11:33:49 -0300

    allow all methods

--- pricing-api/src/main.py
+++ pricing-api/src/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET"],
+    allow_methods=["*"], # allow all methods
     allow_headers=["*"],
 )
 
